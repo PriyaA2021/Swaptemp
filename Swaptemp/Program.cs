1bca4dd1e68bd779ac92e901652d52c1d9aff22f
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 12:16:40 +1200

    Add project files.

---
+++ Swaptemp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Swaptemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            Console.WriteLine("Enter a,b");
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Before Swap a=" + a + " b=" +b);
+            c = a;
+            a = b;
+            b= c;
+
+            
+            Console.WriteLine("After swap a=" + a + " b=" +b);
+
+            
+        }
+    }
+}
